0ef894ce94c37eda9276452066ec3bf14166d597
Ars Vladimirov <grokhael@gmail.com>
2023-06-15 00:12:24 +0400

    change fullname

--- src/settings.py
+++ src/settings.py
@@ -6,7 +6,7 @@ from dataset_tools.templates import AnnotationType, CVTask, Industry, License
 # * Before uploading to instance #
 ##################################
 PROJECT_NAME: str = "RoCoLe"
-PROJECT_NAME_FULL: str = "A robusta coffee leaf images dataset"
+PROJECT_NAME_FULL: str = "RoCoLe: A Robusta Coffee Leaf Images Dataset"
 
 ##################################
 # * After uploading to instance ##

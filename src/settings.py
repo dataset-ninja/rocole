4797b4680af2e0d1569ba343d402b3a375c54518
Ars Vladimirov <grokhael@gmail.com>
2023-07-20 18:11:07 +0600

    upd custom data

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,14 @@
 from typing import Dict, List, Optional, Union
 
-from dataset_tools.templates import AnnotationType, CVTask, Industry, License, Research
+from dataset_tools.templates import (
+    AnnotationType,
+    Category,
+    CVTask,
+    Domain,
+    Industry,
+    License,
+    Research,
+)
 
 ##################################
 # * Before uploading to instance #
@@ -12,14 +20,22 @@ PROJECT_NAME_FULL: str = "RoCoLe: A Robusta Coffee Leaf Images Dataset"
 # * After uploading to instance ##
 ##################################
 LICENSE: License = License.CC_BY_4_0()
-APPLICATIONS: List[Industry] = [Industry.Agriculture(), Research.Biological()]
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [
+    Research.Agricultural(),
+    Research.Biological(),
+]
+CATEGORY: Category = Category.Agriculture()
+
 CV_TASKS: List[CVTask] = [
     CVTask.SemanticSegmentation(),
     CVTask.ObjectDetection(),
 ]
 ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.SemanticSegmentation()]
 
-RELEASE_YEAR: int = 2019
+RELEASE_DATE: Optional[str] = "2019-05-17"  # e.g. "YYYY-MM-DD"
+if RELEASE_DATE is None:
+    RELEASE_YEAR: int = None
+
 HOMEPAGE_URL: str = "https://data.mendeley.com/datasets/c5yvn32dzg/2"
 # e.g. "https://some.com/dataset/homepage"
 
@@ -50,6 +66,13 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = {
 
 PAPER: Optional[str] = "https://www.sciencedirect.com/science/article/pii/S2352340919307693"
 CITATION_URL: Optional[str] = "https://data.mendeley.com/datasets/c5yvn32dzg/2"
+AUTHORS: Optional[List[str]] = [
+    "Jorge Parraga-Alava",
+    "Kevin Cusme",
+    "Angélica Loor",
+    "Esneider Santander",
+]
+
 ORGANIZATION_NAME: Optional[Union[str, List[str]]] = [
     "Escuela Superior Politecnica Agropecuaria de Manabi, Equador",
     "Universidad de Santiago de Chile",
@@ -58,6 +81,7 @@ ORGANIZATION_URL: Optional[Union[str, List[str]]] = [
     "http://www.espam.edu.ec/",
     "https://www.usach.cl/",
 ]
+SLYTAGSPLIT: Optional[Dict[str, List[str]]] = None
 TAGS: List[str] = None
 
 ##################################
@@ -72,11 +96,15 @@ def check_names():
 
 
 def get_settings():
+    if RELEASE_DATE is not None:
+        global RELEASE_YEAR
+        RELEASE_YEAR = int(RELEASE_DATE.split("-")[0])
+
     settings = {
         "project_name": PROJECT_NAME,
-        "project_name_full": PROJECT_NAME_FULL,
         "license": LICENSE,
         "applications": APPLICATIONS,
+        "category": CATEGORY,
         "cv_tasks": CV_TASKS,
         "annotation_types": ANNOTATION_TYPES,
         "release_year": RELEASE_YEAR,
@@ -84,15 +112,20 @@ def get_settings():
         "preview_image_id": PREVIEW_IMAGE_ID,
         "github_url": GITHUB_URL,
     }
+
     if any([field is None for field in settings.values()]):
         raise ValueError("Please fill all fields in settings.py after uploading to instance.")
 
+    settings["release_date"] = RELEASE_DATE
+    settings["project_name_full"] = PROJECT_NAME_FULL or PROJECT_NAME
     settings["download_original_url"] = DOWNLOAD_ORIGINAL_URL
     settings["class2color"] = CLASS2COLOR
     settings["paper"] = PAPER
     settings["citation_url"] = CITATION_URL
+    settings["authors"] = AUTHORS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
-    settings["tags"] = TAGS if TAGS is not None else []
+    settings["slytagsplit"] = SLYTAGSPLIT
+    settings["tags"] = TAGS
 
     return settings
